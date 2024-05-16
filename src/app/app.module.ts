1a28b934337341e4599788a71ade5356ad748671
Laka Nodia <laka.nodia@gmail.com>
2024-05-16 12:48:51 +0400

    Add signup service endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AuthRoutingModule } from './auth/auth-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AuthRoutingModule } from './auth/auth-routing.module';
     AppRoutingModule,
     AuthModule,
     AuthRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,24 +9,46 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit{
 
-  signupForm!: FormGroup;
+  signUpForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
-
-
-  signup(): void {
-    // Implement sign up logic
-  }
-
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
+    this.signUpForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-
-
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      age: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      address: [''],
+      phone: [''],
+      zipcode: [''],
+      avatar: [''],
+      gender: ['MALE']
     });
   }
+
+  signUp(): void {
+    if (this.signUpForm.valid) {
+      this.authService.signUp(this.signUpForm.value)
+        .subscribe(
+          response => {
+            console.log('Sign up successful:', response);
+            alert("Sign up successful");
+            // Handle success, e.g., redirect to login page
+          },
+          error => {
+            alert(error.error.errorKeys);
+            console.error('Sign up failed:', error);
+            // Handle error, e.g., display error message
+          }
+        );
+    }else{
+      alert("ფორმა არავალიდურია !");
+
+    }
+  }
+
+
+
 }
